3756d241f971cad87b0d362e271d97d18c1e4b2d
Daniela Morais <dani.m@hotmail.co.uk>
2016-07-17 03:48:12 -0300

    Encode URL

--- makeRequest/makeRequest.ino
+++ makeRequest/makeRequest.ino
@@ -5,10 +5,8 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 char server[] = "107.191.118.73";
 IPAddress ip(192, 168, 1, 177);
 EthernetClient client;
-int ledStatus = 0; 
 
 void setup() {
-    pinMode(7,OUTPUT); 
   Serial.begin(9600);
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -17,32 +15,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Input:");
-  if (Serial.available() > 0) {
-    ledStatus = Serial.parseInt();
-    Serial.print(ledStatus);
-    if (ledStatus == 1){
-       digitalWrite(7, HIGH);
-    } else { 
-       digitalWrite(7, LOW);
-    }  
-  }
-  
-  delay(7000);
-  
-  if (client.connect(server, 8080) && ledStatus == 1) {
+ 
+  if (client.connect(server, 8080)) {
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /sendTweet/Hello HTTP/1.1");
+    client.println("GET /sendTweet/Hello%20world%20by%20Arduino HTTP/1.1");
     client.println("Host: 107.191.118.73:8080");
     client.println("Connection: close");
     client.println();
-    client.stop();
+client.stop();
   } else {
     Serial.println("connection failed");
   }
   
   delay(5000);
-  ledStatus = 0;
-  digitalWrite(7, LOW);
 }
